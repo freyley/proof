40cc1f07d449deeba084ffee552f4d1797201149
Mikhail Voloshin <mvol@mightydatainc.com>
2020-03-05 19:02:50 -0500

    Adding the JSON file to the Nuxt project.

--- geolife-density-time-grid/www/geolife-spatiotemporal-heatmap/nuxt.config.js
+++ geolife-density-time-grid/www/geolife-spatiotemporal-heatmap/nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '@/plugins/google-maps', mode: 'client' },
   ],
   /*
   ** Nuxt.js dev-modules
--- geolife-density-time-grid/www/geolife-spatiotemporal-heatmap/pages/index.vue
+++ geolife-density-time-grid/www/geolife-spatiotemporal-heatmap/pages/index.vue
@@ -1,85 +1,121 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-      </div>
+  <v-layout>
+    <v-flex>
+
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+        <v-card-title>
+          Geolife Animated Heatmap
         </v-card-title>
+
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
+          <v-alert type="info" v-if="!isDataLoading && !isDataLoaded">
+            The heatmap data has not yet been loaded.
+            The data file is approximately 1 MB JSON.
+            This demo permits you to actively decide to
+            load the data file so that you can get a feel
+            for the speed of the loading process. In
+            practice, the data would begin loading
+            automatically upon the visitor's arrival
+            to the site.
+          </v-alert>
+
+          <div class="map-container">
+
+
+            <GmapMap
+              ref="mapRef"
+              v-if="isDataLoaded"
+              :center="{lat:10, lng:10}"
+              :zoom="7"
+              map-type-id="terrain"
+              style="width: 100%; height: 100%"
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+            </GmapMap>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
         </v-card-text>
+
         <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
+          <v-btn color="primary" v-if="!isDataLoaded" :loading="isDataLoading"
+              @click="loadData()">
+            Load Data
+          </v-btn>
+
+          <v-alert type="info" v-if="dataLoadTimeStart && dataLoadTimeEnd">
+            Data loaded in
+            {{dataLoadTimeEnd.getTime() - dataLoadTimeStart.getTime()}} ms
+          </v-alert>
+
+          <v-btn @click="panToSingapore">
+            Pan to Singapore
           </v-btn>
         </v-card-actions>
       </v-card>
+
     </v-flex>
   </v-layout>
 </template>
 
+<style scoped lang="scss">
+#covid19riskapp {
+  .map-container {
+    background: yellow;
+    width: 100%;
+    height: 50vh;
+  }
+}
+</style>
+
 <script>
 export default {
   components: {
+  },
+
+  data() {
+    return {
+      isDataLoaded: false,
+      isDataLoading: false,
+      errorMsg: null,
+
+      dataLoadTimeStart: null,
+      dataLoadTimeEnd: null,
+
+      googleMapObject: null
+    }
+  },
+
+
+  methods: {
+    loadData() {
+      this.dataLoadTimeStart = new Date();
+      this.dataLoadTimeEnd = null;
+      this.isDataLoading = true;
+      this.isDataLoaded = false;
+      this.errorMsg = null;
+
+      // We have to do this silly timeout trick because we can't
+      // grab a reference to the mapRef element because it doesn't
+      // exist yet because the v-if hasn't processed yet.
+      // And we can't make it exist before the v-if because there's
+      // a bug in the component that throws an annoying DOM error.
+      setTimeout(() => {
+        this.isDataLoaded = true;
+        this.isDataLoading = false;
+        this.dataLoadTimeEnd = new Date();
+
+        setTimeout(() => {
+          this.$refs.mapRef.$mapPromise.then((map) => {
+            this.googleMapObject = map;
+          })
+        }, 0);
+      }, 500);
+    },
+
+    panToSingapore() {
+      if (!this.googleMapObject) {
+        return;
+      }
+      this.googleMapObject.panTo({lat: 1.38, lng: 103.80})
+    }
   }
 }
 </script>
